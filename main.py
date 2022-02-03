4d659353952d875152961e533c549c0dde668eae
a cute duck <i-am@maskduck.ninja>
2022-02-03 18:50:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ routes = [Route('/', ping, methods=['POST'])]
 
 
 app = Starlette(routes=routes)
-uvicorn.run('main:app')
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
 
