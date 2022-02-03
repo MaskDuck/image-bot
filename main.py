36b1f3f51cd29d28a4dba869e77b9e0559b5a37a
a cute duck <i-am@maskduck.ninja>
2022-02-03 18:30:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from starlette.applications import Starlette
 from starlette.routing import Route
-
+import uvicorn
 
 from app import ping
 
@@ -8,5 +8,5 @@ routes = [Route('/', ping, methods=['POST'])]
 
 
 app = Starlette(routes=routes)
-
+uvicorn.run('main:app')
 
