ffd9cff5c6f4bb46126a1e4444a0645adea93767
a cute duck <i-am@maskduck.ninja>
2022-02-03 19:00:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ routes = [Route('/', ping, methods=['POST'])]
 
 app = Starlette(routes=routes)
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
 
