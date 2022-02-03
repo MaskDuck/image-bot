4a8f8bbaff05d131efff3d47573540a5831d1f3e
Minh Dương <i-am@maskduck.ninja>
2022-02-03 20:44:20 +0700

    fix with double quote

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Sanic('idk')
 
 @app.route('/', methods=["POST"])
 async def handler(r):
-    return json({'type': 1})
+    return json({"type": 1})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
