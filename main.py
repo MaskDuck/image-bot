62f3f850263b56dbf3ae52681b8fe6a86b6906e5
Minh Dương <i-am@maskduck.ninja>
2022-02-03 20:32:27 +0700

    fix

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from sanic.response import json
 
 app = Sanic('idk')
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 async def handler(r):
-    return json({'type':1})
+    return json({'type': 1})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
