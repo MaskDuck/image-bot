f76fd70b2d0a48eaab544b501fd95aebe0bc7580
Minh Dương <i-am@maskduck.ninja>
2022-02-04 19:04:17 +0700

    revert

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+from sanic import Sanic
+from sanic.response import json, HTTPResponse
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
 import os
 
-from flask import Flask
-from flask_discord_interactions import DiscordInteractions
-
-
-app = Flask(__name__)
-discord = DiscordInteractions(app)
-app.config["DISCORD_CLIENT_ID"] = os.environ["DISCORD_CLIENT_ID"]
-app.config["DISCORD_PUBLIC_KEY"] = os.environ["DISCORD_PUBLIC_KEY"]
-app.config["DISCORD_CLIENT_SECRET"] = os.environ["DISCORD_CLIENT_SECRET"]
-@discord.command()
-def ping(ctx):
-    return "Pong!"
-
-discord.set_route("/interaction")
-discord.update_commands()
-
-if __name__ == "__main__":
-    app.run()
+app = Sanic('idk')
+PUBLIC_KEY = 'f964c9bf508247bce404a6d6069fb494fc56b276954253389723301bfe503f58'
+@app.route('/', methods=["POST"])
+async def handler(r):
+    verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+    signature = r.headers["X-Signature-Ed25519"]
+    timestamp = r.headers["X-Signature-Timestamp"]
+    body = r.body.decode('utf-8')
+    try:
+        verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+        return json({"type": 1})
+    except:
+        return HTTPResponse(status=401)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
